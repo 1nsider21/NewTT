9c09b82e35cc5bbc932bf9411b15e7d2844a56ea
Artur <artroyal.pro@gmail.com>
2020-10-26 23:12:07 +0200

    cube added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// alert(1);
+'use strict';
+
+const cube = document.querySelector('.cube');
+
+const rotate = () => {
+  const rotateY = 180;
+
+  cube.style.transform = `rotateY(${rotateY}deg)`;
+};
+
+cube.addEventListener('click', rotate);
